19280556c272ce939f712b93bcd29647c2df1584
tahor <tahor.ram@gmail.com>
2020-06-06 20:20:53 +0300

    first commit

--- app/src/main/java/com/spartapps/livedata/http/MovieClientInstance.java
+++ app/src/main/java/com/spartapps/livedata/http/MovieClientInstance.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MovieClientInstance {
 
-    public final static String api_key = "daf3601be8f6ec079d453f37d23189a1";
+    public final static String api_key = "YOUR_API_KEY";
     public final static String photo_url = "https://image.tmdb.org/t/p/w500/";
 
     private static final Retrofit retrofit = new Retrofit.Builder()
